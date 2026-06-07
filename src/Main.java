4da48d6507b72d32e0de14c2cc207b5d8c10650e
Mayara Silva <devmayara11@gmail.com>
2026-06-07 12:03:27 -0300

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     static void main(String[] args) {
         System.out.println("Hello World.");
+        String msg = "Programando em ";
+        System.out.println(msg + args[0]);
+        System.out.println("Usando " + args[1]);
     }
 }
