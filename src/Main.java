167a4c73fd5ebab3468b4c7dd87e863fff31e326
Mayara Silva <devmayara11@gmail.com>
2026-06-07 12:33:57 -0300

    name + age

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     static void main(String[] args) {
-        System.out.println("Hello World.");
-        String msg = "Programando em ";
-        System.out.println(msg + args[0]);
-        System.out.println("Usando " + args[1]);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Olá, informe o seu nome: ");
+        String name = scanner.next();
+
+        System.out.println("Olá, informe a sua idade: ");
+        int age = scanner.nextInt();
+
+//        System.out.println("Olá, " + name + "sua idade é " + age);
+        System.out.printf("Olá, %s sua idade é %s", name, age);
     }
 }
