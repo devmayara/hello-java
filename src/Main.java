e5c694175baccb546502994691b784b37665080a
Mayara Silva <devmayara11@gmail.com>
2026-06-23 20:29:38 -0300

    records

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     static void main(String[] args) {
-        var male = new Person();
-        male.setName("jhon");
-        male.setAge(12);
-
-        var female = new Person();
-        female.setName("mary");
-        female.setAge(14);
-
-        System.out.println("Male:\n name: " + male.getName() + "; age: " + male.getAge());
-        System.out.println("Female:\n name: " + female.getName() + "; age: " + female.getAge());
+        var person = new Person("jhon", 12);
+        System.out.println(person.toString());
     }
 }
